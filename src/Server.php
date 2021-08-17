8c31f2c99a3eb9ebd8a996897391480b418b66b5
gongyuanlin <gong86627@qq.com>
2021-08-17 09:36:37 +0800

    commit a test app

--- src/Server.php
+++ src/Server.php
@@ -3,13 +3,12 @@
 
 namespace gong86627\bobo;
 
-
 class Server
 {
     /**
      * @return string
      */
-    public function info(): string
+    public static function info(): string
     {
         return strtoupper('hello world!');
     }
